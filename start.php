ddf3d5b2f52f050b07d7fa158df8b0ad1ddaaf47
vicktpa <74816922+vicktpa@users.noreply.github.com>
2024-12-07 13:17:38 +0300

    Update start.php

--- start.php
+++ start.php
@@ -1,10 +1,10 @@
 <?php
 
-if( isset( $_GET\[ 'Submit' \] ) ) {
-	$id = $_GET\[ 'id' \];
+if( isset( $_GET['Submit'] ) ) {
+	$id = $_GET[ 'id'];
 
 	$getid  = "SELECT first_name, last_name FROM users WHERE user_id = '$id';";
-	$result = mysqli_query($GLOBALS\["___mysqli_ston"\],  $getid ); // Removed 'or die' to suppress mysql errors
+	$result = mysqli_query($GLOBALS["___mysqli_ston"],  $getid ); // Removed 'or die' to suppress mysql errors
 
 	$num = @mysqli_num_rows( $result ); // The '@' character suppresses errors
 	if( $num > 0 ) {
